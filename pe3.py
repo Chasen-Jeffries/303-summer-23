948d5b1b39289a1224aa8b2d560d3943fa34b0ce
iamfahad <zelal.kutby@cgu.edu>
2023-06-05 19:14:26 -0700

    added decoder

--- pe3.py
+++ pe3.py
@@ -9,8 +9,15 @@ def encode (input_text, shift):
                   encoded_text += char
     return alphabet, encoded_text
 
-
-
-
-
-                  
+def decode(text, shift):
+    decoded_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                decoded_char = chr((ord(char) - shift - 65) % 26 + 65)
+            else:
+                decoded_char = chr((ord(char) - shift - 97) % 26 + 97)
+        else:
+            decoded_char = char
+        decoded_text += decoded_char
+    return decoded_text
